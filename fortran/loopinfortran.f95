99e290f296772fca672e487cd7d75aab16a04d05
Ankit Khanal <ankitkhanal231@gmail.com>
2026-06-02 22:46:09 +0545

    feat: loop in fortran

---
+++ fortran/loopinfortran.f95
@@ -0,0 +1,14 @@
+program myApp
+
+    implicit none
+    
+    integer :: n
+    integer :: doubled
+
+    do n = 1, 10
+        doubled = n * 2
+        print *, doubled
+    end do
+
+
+end program myApp
