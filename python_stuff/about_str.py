0c90ee28e48a691256bc60f454e7852daa0299da
Ankit Khanal <ankitkhanal231@gmail.com>
2026-06-29 17:16:43 +0545

    feat: add demonstration of ord() and chr() functions for character encoding

---
+++ python_stuff/about_str.py
@@ -0,0 +1,11 @@
+"""
+The built-in function ord() converts a code point from its string form to an integer in the range 0 to 0x10FFFF; chr() converts an integer in the range 0 to 0x10FFFF to the corresponding length 1 string object. str.encode() can be used to convert a str to bytes using the given text encoding, and bytes.decode() can be used to achieve the opposite.
+"""
+
+a = "a"
+
+ord_a = ord(a)  # Output: 97 - The Unicode code point for 'a'.
+print(ord_a)
+
+chr_a = chr(ord_a)  # Output: 'a' - Convert the code point back to the character.
+print(chr_a)
