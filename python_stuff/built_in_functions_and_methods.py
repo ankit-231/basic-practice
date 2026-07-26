96ba0685c9cac9e7de988ce7280b3bbbd9174dd8
Ankit Khanal <ankitkhanal231@gmail.com>
2026-07-26 12:09:20 +0545

    feat: built in functions and methods

---
+++ python_stuff/built_in_functions_and_methods.py
@@ -0,0 +1,9 @@
+"""
+A built-in function object is a wrapper around a C function. Examples of built-in functions are len() and math.sin() (math is a standard built-in module). The number and type of the arguments are determined by the C function.
+"""
+
+"""
+Built in methods are functions that have their __self__ defined.
+
+For example: alist.append(), where alist is a list. alist.__self__ is the alist object itself. It is passed to the C function.
+"""
